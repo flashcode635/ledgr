10f869cb235593449bb2b2b686d3e21ebb117635
flashcode635 <151376451+flashcode635@users.noreply.github.com>
2026-04-06 21:54:50 +0530

    v1 done

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,21 @@
 import Link from 'next/link'
- 
+import notFound  from '@/public/not-found.png'
+import Image from 'next/dist/shared/lib/image-external'
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className='h-screen w-screen flex justify-between'>
+      <div className=' w-full text-nowrap flex-wrap flex flex-col justify-center items-center gap-4 p-10'>
+        <h1 className='text-[110px] font-semibold'>404 - Error</h1>
+        <h2 className='text-2xl'> Page Not Found</h2>
+        <p className='text-gray-600'>Could not find requested resource</p>
+        <button className='bg-violet-700 text-white py-2 px-4 rounded-lg hover:bg-violet-800'>
+          <Link href="/">Return Home</Link>
+        </button>
+      </div>
+      <div className='w-[40%] h-screen p-0 m-0'>
+
+        <Image src={notFound} width={400} alt="Not Found" />
+      </div>
     </div>
   )
 }
